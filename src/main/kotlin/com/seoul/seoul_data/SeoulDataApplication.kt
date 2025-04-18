053df722e1c0dde7c02110764f7b817b32d21099
wxodi0 <wxodi0@github.com>
2025-04-18 22:22:35 +0900

    init

---
+++ src/main/kotlin/com/seoul/seoul_data/SeoulDataApplication.kt
@@ -0,0 +1,11 @@
+package com.seoul.seoul_data
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeoulDataApplication
+
+fun main(args: Array<String>) {
+  runApplication<SeoulDataApplication>(*args)
+}
---
+++ src/test/kotlin/com/seoul/seoul_data/SeoulDataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seoul.seoul_data
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeoulDataApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
